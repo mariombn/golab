228e84ac75428ef7f189e1f0531261f47afb2a71
Mario de Moraes Barros Neto <mariombn@gmail.com>
2023-06-08 22:41:54 +0000

    Base do gin

--- main.go
+++ main.go
@@ -1,44 +1,20 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	ID    int
-	Email string
-}
-
 func main() {
-	// Configuração da conexão com o banco de dados
-	db, err := sql.Open("mysql", "root:secret@tcp(127.0.0.1)/perfect_pay")
-	if err != nil {
-		log.Fatal("Erro ao abrir conexão com o banco de dados MySQL:", err)
-	}
-	defer db.Close()
+	// Cria uma nova instância do gin
+	router := gin.Default()
 
-	// Consulta ao banco de dados
-	rows, err := db.Query("SELECT id, email FROM users")
-	if err != nil {
-		log.Fatal("Erro ao executar a consulta no banco de dados MySQL:", err)
-	}
-	defer rows.Close()
+	// Rota GET "/hello"
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Olá, Mundo!",
+		})
+	})
 
-	// Iteração sobre as linhas e impressão do ID e nome separados por hífen
-	for rows.Next() {
-		var user User
-		err := rows.Scan(&user.ID, &user.Email)
-		if err != nil {
-			log.Fatal("Erro ao ler dados da linha:", err)
-		}
-		fmt.Printf("ID: %d - E-mail: %s\n", user.ID, user.Email)
-	}
-
-	if err := rows.Err(); err != nil {
-		log.Fatal("Erro ao ler todas as linhas:", err)
-	}
+	// Inicia o servidor HTTP
+	router.Run(":8080")
 }
